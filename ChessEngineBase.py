4de36fe7b0b279058d230e2aa4daf086244c4f45
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2025-09-04 16:06:45 -0700

    [ADD] base engine class

---
+++ ChessEngineBase.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class ChessEngineBase(ABC):
+    def __init__():
+        pass
+    def handleInput(command:str):
+        pass
+    def handleOutput():
+        pass
+    @abstractmethod
+    def determineBestMove():
+        pass
+    @abstractmethod
+    def analyze():
+        pass
+
